4e26a19ac5a7b685d4ca7ae4eaf7c6650248e104
stupidfool <bhargav.satya16@gmail.com>
2018-04-11 12:07:33 +0000

    ADD TEST VALUES TO DATABASE

--- database/cart.sql
+++ database/cart.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE cart (
 
-username VARCHAR(50) NOT NULL UNIQUE,
+username VARCHAR(50) NOT NULL ,
 
 vegname VARCHAR(50) NOT NULL,
 
@@ -19,3 +19,20 @@ PRIMARY KEY (username,vegname,dealerid,region,paymentat)
 
 );
 
+
+select * from cart as c inner  join stock as s on c.vegname=s.vegname and c.dealerid=s.dealerid and c.region=s.region ;
+select username,dealerid,region,sum(price) from cart as c inner  join stock as s on c.vegname=s.vegname and c.dealerid=s.dealerid and c.region=s.region group by vegname;
+
+
+insert into cart (username,vegname,dealerid,region,quantity) values ('dedsec','Tomato','retailer1','Hyderabad',4);
+insert into cart (username,vegname,dealerid,region,quantity) values ('dedsec','Potato','retailer2','Hyderabad',2);
+insert into cart (username,vegname,dealerid,region,quantity) values ('satya','Tomato','retailer1','Allahabad',4);
+insert into cart (username,vegname,dealerid,region,quantity) values ('satya','Cabbage','retailer3','Allahabad',2);
+insert into cart (username,vegname,dealerid,region,quantity) values ('satya','Potato','retailer4','Allahabad',1);
+
+
+insert into cart  values ('dedsec','Tomato','retailer1','Hyderabad',4,'yes',CURRENT_TIMESTAMP);
+insert into cart  values ('dedsec','Potato','retailer2','Hyderabad',2,'yes',CURRENT_TIMESTAMP);
+insert into cart values ('sandeep','Tomato','retailer1','Mumbai',1,'yes',CURRENT_TIMESTAMP);
+insert into cart values ('sandeep','Onion','retailer2','Mumbai',2,'yes',CURRENT_TIMESTAMP);
+insert into cart values ('sandeep','Potato','retailer4','Mumbai',3,'yes',CURRENT_TIMESTAMP);
--- database/payment.sql
+++ database/payment.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE payment (
 
-username VARCHAR(50) NOT NULL UNIQUE,
+username VARCHAR(50) NOT NULL ,
 
 paymentat DATETIME  DEFAULT '1000-10-10 00:00:00' ,
 
@@ -22,3 +22,7 @@ isprimaryaddress  VARCHAR(255) DEFAULT 'yes',
 PRIMARY KEY (username,paymentat)
 
 );
+
+
+insert into payment (username,paymentat,price) values('dedsec','2018-04-11 11:32:07',112);
+insert into payment (username,paymentat,price) values('sandeep','2018-04-11 11:32:13',103);
