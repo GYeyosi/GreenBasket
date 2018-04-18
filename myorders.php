45f463cbca43da571494c13254fd100eb5749d20
stupidfool <bhargav.satya16@gmail.com>
2018-04-18 14:18:36 +0000

    orders

--- myorders.php
+++ myorders.php
@@ -69,7 +69,7 @@ if (mysqli_num_rows($result) ) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link href="./css/style.css" rel="stylesheet" type="text/css">
       <script src="https://use.fontawesome.com/07b0ce5d10.js"></script>
-      >
+      
     
 
 
@@ -78,12 +78,214 @@ if (mysqli_num_rows($result) ) {
     
 
     <link rel="stylesheet" href="../css/footer.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link href="./css/style.css" rel="stylesheet" type="text/css">
+      <script src="https://use.fontawesome.com/07b0ce5d10.js"></script>
+       <link href="./css/footer.css" rel="stylesheet"/>
+      <script src=".js/footer.js"></script>
     
 
   
   </head>
 
   <body>
+   
+      <!--=========-TOP_BAR============-->
+      <nav class="topBar">
+         <div class="container">
+            <ul class="list-inline pull-left hidden-sm hidden-xs">
+              <li>  <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'bn,en,gu,hi,pa,ta,te', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</li>
+               <li><span class="text-primary">Have a question? </span> Call +120 558 7885</li>
+                 <li>           <h3>An e-Vegetable Market</h3></li>
+            </ul>
+            <ul class="topBarNav pull-right">
+               <!-- <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false"> <i class="fa fa-usd mr-5"></i>USD<i class="fa fa-angle-down ml-5"></i>
+                  </a>
+                  <ul class="dropdown-menu w-100" role="menu">
+                    <li><a href="#"><i class="fa fa-eur mr-5"></i>EUR</a>
+                    </li>
+                    <li class=""><a href="#"><i class="fa fa-usd mr-5"></i>USD</a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-gbp mr-5"></i>GBP</a>
+                    </li>
+                  </ul>
+                  </li> -->
+               <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false"> <span class="hidden-xs"> More <i class="fa fa-angle-down ml-5"></i></span> </a>
+                  <ul class="dropdown-menu w-100" role="menu">
+                      <li>
+                        <a href="./profile/profile.php">Edit Profile</a>
+                     </li>
+                     <li>
+                        <a href="./cart/cart.php">Your Orders</a>
+                     </li>
+                     <li>
+                        <a href="/sellOnBasket/sell.php">Sell on GreenBasket</a>
+                     </li>
+                     <li>
+                        <a href="#">Contact Us</a>
+                     </li>
+                  </ul>
+               </li>
+               <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false"> <i class="fa fa-user mr-5"></i><span class="hidden-xs">My Account<i class="fa fa-angle-down ml-5"></i></span> </a>
+                  <ul class="dropdown-menu w-150" role="menu">
+
+                    <li><a href="./profile/profile.php"><?php echo $user ?> </a>
+
+                     </li>
+                    <li><a href="cart.html">My Orders</a>
+                     </li>
+                     <li><a href="./login/logout.php">Logout</a>
+                     </li>
+                     <li class="divider"></li>
+                     <li><a href="./cart/cart.php">Wishlist</a>
+                     </li>
+                     <li><a href="./cart/cart.php">My Cart</a>
+                     </li>
+                     <li><a href="checkout.php">Checkout</a>
+                     </li>
+                  </ul>
+               </li>
+               <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false"> <i class="fa fa-shopping-basket mr-5"></i> <span class="hidden-xs">
+                  Cart <i class="fa fa-angle-down ml-5"></i>
+                  </span> </a>
+                  <ul class="dropdown-menu w-150" role="menu">
+                     <li><a href="./cart/cart.php">View Cart</a>
+                     </li>
+                     <li><a href="checkout.php">Check Out</a>
+                     </li>
+                  </ul>
+               </li>
+            </ul>
+         </div>
+      </nav>
+      <!--=========-TOP_BAR============-->
+      <!--=========MIDDEL-TOP_BAR============-->
+      <div class="middleBar">
+         <div class="container">
+            <div class="row display-table">
+               <div class="col-sm-3 vertical-align text-left hidden-xs">
+                  <a href="./index.php"><img width="180px" src="./img/logo.png" alt="Green Basket"></a>
+                 
+               </div>
+               <!-- end col -->
+               <div class="col-sm-7 vertical-align text-center">
+                  <form>
+                     <div class="row grid-space-1">
+                        <div class="col-sm-6">
+                           <input type="text" name="keyword" class="form-control input-lg" placeholder="Search">
+                        </div>
+                        <!-- end col -->
+                        <div class="col-sm-3">
+                           <select class="form-control input-lg" name="category">
+                              <option value="all">Categories</option>
+                              <optgroup label="Vegetables">
+                                 <option value="tomato">Tomato</option>
+                                 <option value="potato">Potato</option>
+                                 <option value="ladys-finger">Lady's Finger</option>
+                                 <option value="brinjals">Brinjals</option>
+                                 <option value="carrot">Carrot</option>
+                                 <option value="cucumber">Cucumber</option>
+                              </optgroup>
+                              <optgroup label="Daily Vegetables">
+                                 <option value="onion">Onioins</option>
+                                 <option value="Garlic">Garlic</option>
+                                 <option value="ginger">Ginger</option>
+                                 </optgroup>
+                              <optgroup></optgroup>
+                           </select>
+                        </div>
+                        <!-- end col -->
+                        <div class="col-sm-3">
+                           <input type="submit" class="btn btn-default btn-block btn-lg" value="Search">
+                        </div>
+                        <!-- end col -->
+                     </div>
+                     <!-- end row -->
+                  </form>
+               </div>
+               <!-- end col -->
+               <!-- end col -->
+            </div>
+            <!-- end  row -->
+         </div>
+      </div>
+      <nav class="navbar navbar-main navbar-default" role="navigation" style="opacity: 1;">
+         <div class="container">
+            <!-- Brand and toggle -->
+            <div class="navbar-header">
+               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-1">
+               <span class="sr-only">Toggle navigation</span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               </button>             
+            </div>
+            <!-- Collect the nav links,  -->
+            <div class="collapse navbar-collapse navbar-1" style="margin-top: 0px;">
+               <ul class="nav navbar-nav">
+                  <li><a href="./index.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">Home</a></li>
+                  <li class="dropdown megaDropMenu">
+                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">Shop <i class="fa fa-angle-down ml-5"></i></a>
+                     <ul class="dropdown-menu row">
+                        <li class="col-sm-3 col-xs-12">
+                           <ul class="list-unstyled">
+                              <li>Products Grid View</li>
+                              <li><a href="#">Products</a></li>
+                              <li><a href="#">Sidebar Left</a></li>
+                              <li><a href="#">Products Left</a></li>
+                           </ul>
+                        </li>
+                        <li class="col-sm-3 col-xs-12">
+                           <ul class="list-unstyled">
+                              <li>Products List View</li>
+                              <li><a href="#"> Sidebar Left</a></li>
+                              <li><a href="#">Products Left</a></li>
+                              <li><a href="#">Products Sidebar</a></li>
+                           </ul>
+                        </li>
+                        <li class="col-sm-3 col-xs-12">
+                           <ul class="list-unstyled">
+                              <li>Checkout</li>
+                              <li><a href="#">Step 1</a></li>
+                              <li><a href="#">Step 2</a></li>
+                              <li><a href="#">Step 3</a></li>
+                           </ul>
+                        </li>
+                        <!--
+                        <li class="col-sm-3 col-xs-12">
+                           <ul class="list-unstyled">
+                              <li>Sumit Kumar</li>
+                           </ul>
+                           <img src="https://lh3.googleusercontent.com/-uwagl9sPHag/WM7WQa00ynI/AAAAAAAADtA/hio87ZnTpakcchDXNrKc_wlkHEcpH6vMwCJoC/w140-h148-p-rw/profile-pic.jpg" class="img-responsive" alt="menu-img">
+                        </li>  -->
+                     </ul>
+                  </li>
+                  <li class="dropdown">
+                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">Page <i class="fa fa-angle-down ml-5"></i></a>
+                     <ul class="dropdown-menu dropdown-menu-left">
+                        <li><a href="./login/logout.php">Logout</a></li>
+                        <li><a href="#">Password Recovery</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">About Us</a></li>
+                     </ul>
+                  </li>
+                  <li><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">Help-Section</a></li>
+                  <li><a href="./feedback_form/formpage.html" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">FeedBack</a></li>
+               </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+         </div>
+      </nav> 
 <div class="panel panel-default">
   <div class="panel-heading"style="background-color:  #095f59;">
     <h3 class="panel-title">  All Users </h3>
@@ -98,7 +300,7 @@ if (mysqli_num_rows($result) ) {
         <th>Paymentat</th>
       </tr>
       <?php
-        $result = mysqli_query($link,"select c.username,sum(c.quantity*s.price) as total,c.paymentstatus,c.paymentat from cart c  join (select  dealerid,vegname,region,price from stock) s on c.dealerid=s.dealerid and c.vegname=s.vegname and c.region=s.region where  paymentstatus='yes'  group by c.username,paymentstatus,paymentat;");
+        $result = mysqli_query($link,"select c.username,sum(c.quantity*s.price) as total,c.paymentstatus,c.paymentat from cart c  join (select  dealerid,vegname,region,price from stock) s on c.dealerid=s.dealerid and c.vegname=s.vegname and c.region=s.region where username='$user'  and paymentstatus='yes'  group by c.username,paymentstatus,paymentat;");
         if (mysqli_num_rows($result)) {
 
             // output data of each row
@@ -120,5 +322,82 @@ if (mysqli_num_rows($result) ) {
 
   </div>
 </div>
+<!-- START OF FOOTER -->
+<div class="footer-section">
+    <div class="footer">
+   <div class="container">
+          <div class="col-md-4 footer-one">
+            <div class="foot-logo">
+                <img src="./img/logo.png">
+            </div> 
+             
+             <p>Providing Life Changing Experiences Through Education. Class That Fit Your Busy Life. Closer to Home
+                </p>
+            <div class="social-icons"> 
+               <a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+               <a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+               <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+               <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+           </div>
+          </div>
+          <div class="col-md-2 footer-two">
+             <h5>Quick Links</h5>
+                <ul>
+                    <li><a href="#"> About Us</a> </li>
+                    <li><a href="#"> Our News</a> </li>
+                    <li><a href="#"> Our Services</a> </li>
+                    <li><a href="#"> Contact Us</a> </li>
+                  </ul>
+                  
+          </div>
+          <div class="col-md-2 footer-three">
+             <h5>Services</h5>
+                <ul>
+                    <li><a href="#"> About Us</a> </li>
+                    <li><a href="#"> Our News</a> </li>
+                    <li><a href="#"> Our Services</a> </li>
+                    <li><a href="#"> Contact Us</a> </li>
+                  </ul>
+                  
+          </div>
+          <div class="col-md-4 footer-four">
+             <h5>Contact Us</h5>
+                <ul>
+                    <li><i class="fa fa-map-marker"></i>350 Avenue, India, Delhi 110001 </li>
+                    <li><i class="fa fa-envelope-o"></i>info@mailme.com </li>
+                    <li><i class="fa fa-phone"></i>+91-xxx-xxx-2040 </li>
+                    
+                  </ul>
+                  
+          </div>
+          
+      
+      
+      
+      
+      
+      <div class="clearfix"></div>
+   </div>
+</div>
+
+</div>
+<div class="footer-bottom">
+        <div class="container">
+               <div class="row">
+                  <div class="col-sm-6 ">
+                     <div class="copyright-text">
+                        <p>CopyRight © 2017 Digital All Rights Reserved</p>
+                     </div>
+                  </div> <!-- End Col -->
+                  <div class="col-sm-6  ">
+                      <div class="copyright-text pull-right">
+                        <p> <a href="#">Home</a> | <a href="#">Privacy</a> |<a href="#">Terms & Conditions</a> | <a href="#">Refund Policy</a> </p>
+                     </div>               
+                                       
+                  </div> <!-- End Col -->
+               </div>
+            </div>
+    </div>
+   </div>
 </body>
 </html>
